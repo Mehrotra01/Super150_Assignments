7be0e5be8b400e1da715aa8c70fb6284963583af
Harsh Mehrotra <harshmehrotra01@gmail.com>
2022-11-01 18:34:11 +0530

    First Commit

---
+++ Assignment-1/src/PrimePrint.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class PrimePrint {
+    public static void main(String[] args) {
+        Scanner hm = new Scanner(System.in);
+        
+        hm.close();
+    }
+}
